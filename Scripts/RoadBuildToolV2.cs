674ba8267ff9cf3cd5a0babea6cc72dafc2357b7
kotilja <komandanto228@gmail.com>
2026-04-15 23:57:18 +0400

    Update RoadBuildToolV2.cs

--- Scripts/RoadBuildToolV2.cs
+++ Scripts/RoadBuildToolV2.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteAlways]
-public class RoadNodeV2 : MonoBehaviour
+public class RoadBuildToolV2 : MonoBehaviour
 {
     public enum JunctionControlMode
     {
