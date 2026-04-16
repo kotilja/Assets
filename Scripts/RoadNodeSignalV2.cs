fb9ab71941adaccb372648a8e259652f34294ffe
kotilja <komandanto228@gmail.com>
2026-04-16 13:15:42 +0400

    Update RoadNodeSignalV2.cs

--- Scripts/RoadNodeSignalV2.cs
+++ Scripts/RoadNodeSignalV2.cs
@@ -792,6 +792,25 @@ public class RoadNodeSignalV2 : MonoBehaviour
         return false;
     }
 
+    private bool CanUseMovementFallback(RoadSegmentV2 incomingSegment, RoadLaneConnectionV2.MovementType movementType)
+    {
+        if (incomingSegment == null)
+            return false;
+
+        LampState lampState = GetLampStateForSegment(incomingSegment);
+
+        switch (lampState)
+        {
+            case LampState.Green:
+                return MovementTypeAllowed(movementType);
+
+            case LampState.Yellow:
+            case LampState.Red:
+            default:
+                return false;
+        }
+    }
+
     private bool MovementTypeAllowed(RoadLaneConnectionV2.MovementType movementType)
     {
         switch (movementType)
